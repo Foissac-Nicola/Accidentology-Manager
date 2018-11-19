3cf84e21cfbcce3607716d7c21f89ea412c3dd49
Foissac-Nicola <12102598+Foissac-Nicola@users.noreply.github.com>
2018-11-19 12:36:22 +0100

    add setup.py
    wip #8

---
+++ BackEnd/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='BackEnd',
+    version='0.1',
+    packages=['BackEnd'],
+    url='',
+    license='',
+    author='Team Accidentology-Manager',
+    author_email='',
+    description='',
+    install_requires=['flask' , 'flask_restful','flask_sqlalchemy']
+)
