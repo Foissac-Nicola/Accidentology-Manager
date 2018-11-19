f0d1d33e66631257b9e232a3a5a44189ddae654a
Foissac-Nicola <12102598+Foissac-Nicola@users.noreply.github.com>
2018-11-19 12:28:04 +0100

    add driver for postgres
    wip #8

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'postgres',
+    'db': 'my_database',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+db.init_app(app)
 api = Api(app)
 
 class ServiceIndicator(Resource):
@@ -21,3 +37,4 @@ class ServiceIndicator(Resource):
 api.add_resource(ServiceIndicator,'/Indicator')
 if __name__ == '__main__':
     app.run()
+
