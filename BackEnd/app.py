c239820c688c84ae99f8540ea4a4a6a76a356443
Foissac-Nicola <12102598+Foissac-Nicola@users.noreply.github.com>
2018-11-19 12:09:07 +0100

    add example for rest
    wip #8

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -4,12 +4,20 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class ServiceIndicator(Resource):
+
     def get(self):
-        return {'hello': 'world'}
+        return {"get": "example"}
 
-api.add_resource(HelloWorld, '/')
+    def post(self):
+        return {"post": "example"}
 
+    def delete(self):
+        return {"delete": "example"}
 
+    def put(self):
+        return {"put": "example"}
+
+api.add_resource(ServiceIndicator,'/Indicator')
 if __name__ == '__main__':
     app.run()
